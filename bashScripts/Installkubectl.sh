199a46646e32b7408fd45d54204af39ff78d816f
hossamalsankary <hossamalsankary@gmail.com>
2022-12-01 00:48:10 +0200

    update kube

--- bashScripts/Installkubectl.sh
+++ bashScripts/Installkubectl.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-
 sudo apt-get install -y ca-certificates curl
-
 sudo apt-get install -y apt-transport-https
-
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https:/$
-
-echo  "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] ht$
-
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-
 sudo apt-get install -y kubectl
