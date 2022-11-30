306b51525d5af12b4188df80280f255274c79d48
hossamalsankary <hossamalsankary@gmail.com>
2022-12-01 01:34:27 +0200

    update

--- bashScripts/Installkubectl.sh
+++ bashScripts/Installkubectl.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
