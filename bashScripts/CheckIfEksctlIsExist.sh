4c7b1687285d2a9681afb22eb2e385fbc241de30
hossamalsankary <hossamalsankary@gmail.com>
2022-11-30 23:05:16 +0200

    update

--- bashScripts/CheckIfEksctlIsExist.sh
+++ bashScripts/CheckIfEksctlIsExist.sh
@@ -18,6 +18,6 @@ else
 
 echo "install eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
- mv /tmp/eksctl /usr/local/bin
+ sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 fi
