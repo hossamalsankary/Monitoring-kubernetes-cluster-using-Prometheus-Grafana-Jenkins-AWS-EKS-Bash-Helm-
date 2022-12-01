c0915022049477ded119ff5cfed678f7f36accd3
hossamalsankary <hossamalsankary@gmail.com>
2022-12-01 02:04:16 +0200

    update error: exec plugin: invalid apiVersion "client.authentication.k8s.io/v1alpha1"

--- bashScripts/Installkubectl.sh
+++ bashScripts/Installkubectl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
  sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
- sudo chmod +x /bin/linux/amd64/kubectl
  sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
- sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+ # for fix the (error: exec plugin: invalid apiVersion "client.authentication.k8s.io/v1alpha1")
+sed -i -e "s,client.authentication.k8s.io/v1alpha1,client.authentication.k8s.io/v1beta1,g" ~/.kube/config
